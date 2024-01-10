2319566811fa1001cf80bb2f5007ab527fa0974a
shrobonbharui <shrobonbharui@gmail.com>
2024-01-10 23:32:06 +0530

    add javaScript

---
+++ script.js
@@ -0,0 +1,7 @@
+const userInput = document.getElementById('userInput');
+const addButton = document.getElementById('addButton');
+const taskCount = document.getElementById('taskCount');
+const allCompleteButton = document.getElementById('allCompleteButton');
+const allClearButton = document.getElementById('allClearButton');
+const taskList = document.getElementById('taskList');
+
